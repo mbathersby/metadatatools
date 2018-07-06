4c2dc12dee297f516b7e008ee31b66471f9898ba
Matthew Bathersby <matthew.bathersby@accenture.com>
2018-07-06 13:31:17 +1000

    commit

--- js/mdTools.js
+++ js/mdTools.js
@@ -1,13 +1,15 @@
-function login(instance) {
-	var loginUrl = 'https://' + instance + '.salesforce.com';
+({
+	login: function (instance) {
+		var loginUrl = 'https://' + instance + '.salesforce.com';
 
-	jsforce.browser.init({
-		loginUrl: loginUrl,
-		clientId: '3MVG9d8..z.hDcPI8U4xIar0rbAfGvpz7BlQxnsOysVaE4_ZcC9zCoNIbxYE.mMWcvnwcZJ.darnhxzlfTWtG',
-		redirectUri: 'https://metadatatoolkit.herokuapp.com/authorize.htm',
-		proxyUrl: 'https://node-salesforce-proxy.herokuapp.com/proxy/'
-	});
+		jsforce.browser.init({
+			loginUrl: loginUrl,
+			clientId: '3MVG9d8..z.hDcPI8U4xIar0rbAfGvpz7BlQxnsOysVaE4_ZcC9zCoNIbxYE.mMWcvnwcZJ.darnhxzlfTWtG',
+			redirectUri: 'https://metadatatoolkit.herokuapp.com/authorize.htm',
+			proxyUrl: 'https://node-salesforce-proxy.herokuapp.com/proxy/'
+		});
 
-	jsforce.browser.login();
+		jsforce.browser.login();
+	}
 
-}
+})
