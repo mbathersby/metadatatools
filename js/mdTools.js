a0176719103885ee333bf4c6bfa3373c9d412201
Matthew Bathersby <matthew.bathersby@accenture.com>
2018-07-12 01:01:46 +1000

    commit

--- js/mdTools.js
+++ js/mdTools.js
@@ -55,11 +55,11 @@ function buildTable(tableId, cols, fields){
 
 			
 			for(var j = 0; j < fields.length; j++){
-				if(field[j].updateable){
-					if(fields[j].name == cols[i] || fields[j].label){
-						tableRow += '<option class="slds-p-around_xx-small" data-type="' + field[j].type+ '" text="' + field[j].label + '(' + field[j].type+ ')" value="' + field[j].name + '" selected />';
+				if(fields[j].updateable){
+					if(fields[j].name == cols[i] || fields[j].label == cols[i]){
+						tableRow += '<option class="slds-p-around_xx-small" data-type="' + fields[j].type+ '" text="' + fields[j].label + '(' + fields[j].type+ ')" value="' + fields[j].name + '" selected />';
 					} else {
-						tableRow += '<option class="slds-p-around_xx-small" data-type="' + field[j].type+ '" text="' + field[j].label + '(' + field[j].type+ ')" value="' + field[j].name + '" />';
+						tableRow += '<option class="slds-p-around_xx-small" data-type="' + fields[j].type+ '" text="' + fields[j].label + '(' + fields[j].type+ ')" value="' + fields[j].name + '" />';
 					}
 				}
 			}
