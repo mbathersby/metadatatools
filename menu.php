052e94305aba85a6ce346759c2eebfed230821ee
Matthew Bathersby <matthew.bathersby@accenture.com>
2018-07-23 13:01:29 +1000

    menu

--- menu.php
+++ menu.php
@@ -1,4 +1,3 @@
-<div style="height: 16rem;">
 	<div class="slds-context-bar">
 		<div class="slds-context-bar__primary">
 			<div class="slds-context-bar__item slds-context-bar__dropdown-trigger slds-dropdown-trigger slds-dropdown-trigger_click slds-no-hover">
@@ -9,6 +8,7 @@
 		</div>
 		<nav class="slds-context-bar__secondary" role="navigation">
 			<ul class="slds-grid">
+			
 				<li class="slds-context-bar__item slds-is-active">
 					<a href="javascript:void(0);" class="slds-context-bar__label-action" title="Home">
 						<span class="slds-assistive-text">Current Page:</span>
@@ -17,63 +17,57 @@
 				</li>
 				<li class="slds-context-bar__item slds-context-bar__dropdown-trigger slds-dropdown-trigger slds-dropdown-trigger_hover">
 					<a href="javascript:void(0);" class="slds-context-bar__label-action" title="Menu Item">
-						<span class="slds-truncate" title="Menu Item">Custom Metadata Tools</span>
+						<span class="slds-truncate" title="Menu Item">sObjects</span>
 					</a>
 					<div class="slds-context-bar__icon-action slds-p-left_none">
 						<button class="slds-button slds-button_icon slds-button_icon slds-context-bar__button" aria-haspopup="true" title="Open menu item submenu">
 							<svg class="slds-button__icon" aria-hidden="true">
-								<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="slds/assets/icons/utility-sprite/svg/symbols.svg#chevrondown" />
+								<use xlink:href="slds/assets/icons/utility-sprite/svg/symbols.svg#chevrondown" />
 							</svg>
-							<span class="slds-assistive-text">Open menu item submenu</span>
 						</button>
 					</div>
 					<div class="slds-dropdown slds-dropdown_right">
 						<ul class="slds-dropdown__list" role="menu">
 							<li class="slds-dropdown__item" role="presentation">
 								<a href="javascript:void(0);" role="menuitem" tabindex="-1">
-									<span class="slds-truncate" title="Main action">
-										<svg class="slds-icon slds-icon_x-small slds-icon-text-default slds-m-right_x-small" aria-hidden="true">
-											<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="slds/assets/icons/utility-sprite/svg/symbols.svg#add" />
-										</svg>Main action</span>
-								</a>
-							</li>
-							<li class="slds-dropdown__header slds-has-divider_top-space" role="separator">
-								<span class="slds-text-title_caps">Menu header</span>
-							</li>
-							<li class="slds-dropdown__item" role="presentation">
-								<a href="javascript:void(0);" role="menuitem" tabindex="-1">
-									<span class="slds-truncate" title="Menu Item One">Menu Item One</span>
+									<span class="slds-truncate" title="Menu Item One">Quick Create</span>
 								</a>
 							</li>
 							<li class="slds-dropdown__item" role="presentation">
 								<a href="javascript:void(0);" role="menuitem" tabindex="-1">
-									<span class="slds-truncate" title="Menu Item Two">Menu Item Two</span>
-								</a>
-							</li>
-							<li class="slds-dropdown__item" role="presentation">
-								<a href="javascript:void(0);" role="menuitem" tabindex="-1">
-									<span class="slds-truncate" title="Menu Item Three">Menu Item Three</span>
+									<span class="slds-truncate" title="Menu Item Two">Delete</span>
 								</a>
 							</li>
 						</ul>
 					</div>
 				</li>
-				<li class="slds-context-bar__item">
+				<li class="slds-context-bar__item slds-context-bar__dropdown-trigger slds-dropdown-trigger slds-dropdown-trigger_hover">
 					<a href="javascript:void(0);" class="slds-context-bar__label-action" title="Menu Item">
-						<span class="slds-truncate" title="Menu Item">Menu Item</span>
+						<span class="slds-truncate" title="Menu Item">Custom Metadata</span>
 					</a>
+					<div class="slds-context-bar__icon-action slds-p-left_none">
+						<button class="slds-button slds-button_icon slds-button_icon slds-context-bar__button" aria-haspopup="true" title="Open menu item submenu">
+							<svg class="slds-button__icon" aria-hidden="true">
+								<use xlink:href="slds/assets/icons/utility-sprite/svg/symbols.svg#chevrondown" />
+							</svg>
+						</button>
+					</div>
+					<div class="slds-dropdown slds-dropdown_right">
+						<ul class="slds-dropdown__list" role="menu">
+							<li class="slds-dropdown__item" role="presentation">
+								<a href="javascript:void(0);" role="menuitem" tabindex="-1">
+									<span class="slds-truncate" title="Menu Item One">Upload</span>
+								</a>
+							</li>
+							<li class="slds-dropdown__item" role="presentation">
+								<a href="javascript:void(0);" role="menuitem" tabindex="-1">
+									<span class="slds-truncate" title="Menu Item Two">Delete</span>
+								</a>
+							</li>
+						</ul>
+					</div>
 				</li>
-				<li class="slds-context-bar__item">
-					<a href="javascript:void(0);" class="slds-context-bar__label-action" title="Menu Item">
-						<span class="slds-truncate" title="Menu Item">Menu Item</span>
-					</a>
-				</li>
-				<li class="slds-context-bar__item">
-					<a href="javascript:void(0);" class="slds-context-bar__label-action" title="Menu Item">
-						<span class="slds-truncate" title="Menu Item">Menu Item</span>
-					</a>
-				</li>
+				
 			</ul>
 		</nav>
 	</div>
-</div>
