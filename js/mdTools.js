2676054de77e31f69453500f577d4f3ce4f879b9
Matthew Bathersby <matthew.bathersby@accenture.com>
2018-07-24 17:04:17 +1000

    commit

--- js/mdTools.js
+++ js/mdTools.js
@@ -284,7 +284,8 @@ function deployZip() {
 					clearInterval(poll);
 					
 					$('#deployState').html(
-						'Components Deployed: ' + reqStatus.numberComponentsDeployed + '<br/>'
+						'<br/>'
+						+ 'Components Deployed: ' + reqStatus.numberComponentsDeployed + '<br/>'
 						+ 'Component Errors: ' + reqStatus.numberComponentErrors
 					
 					);
