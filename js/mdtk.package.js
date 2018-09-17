f6db50615706eb78c81b04c84a8d0bc00b30919b
mbathersby <35317903+mbathersby@users.noreply.github.com>
2018-09-17 16:38:41 +1000

    Update mdtk.package.js

--- js/mdtk.package.js
+++ js/mdtk.package.js
@@ -5,7 +5,7 @@ function setBaseXml(){
     var baseXml = '<?xml version="1.0" encoding="UTF-8"?>\n'
     + '\t<Package xmlns="http://soap.sforce.com/2006/04/metadata">\n'
     + '\n'
-    + '\t<version>' + apiVersion _ '</version>\n'
+    + '\t<version>' + apiVersion + '</version>\n'
     + '</Package>';
 
     $('.xmlOutput').html(baseXml); 
