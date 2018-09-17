cf239ba390eb47f8795d9ca381d1f7b43edb1f7d
mbathersby <35317903+mbathersby@users.noreply.github.com>
2018-09-17 16:21:55 +1000

    Create mdtk.package.js

---
+++ js/mdtk.package.js
@@ -0,0 +1,5 @@
+var skeleton = '<?xml version="1.0" encoding="UTF-8"?>\n'
++ '\t<Package xmlns="http://soap.sforce.com/2006/04/metadata">\n'
++ '\n'
++ '\t<version>41.0</version>\n'
++ '</Package>';
