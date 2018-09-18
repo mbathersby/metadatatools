a52985fa6332a8d9375346f99126a756e6392d4c
mbathersby <35317903+mbathersby@users.noreply.github.com>
2018-09-18 11:15:12 +1000

    Update mdtk.core.js

--- js/mdtk.core.js
+++ js/mdtk.core.js
@@ -1,20 +1,55 @@
 var userInfo, conn;
 var apiVersion = '43.0';
 
+$(document).ready(this, init());
+
 function init() {
 	setNavigation();
+	
 	jsforce.browser.init({
 		clientId: '3MVG9d8..z.hDcPI8U4xIar0rbAfGvpz7BlQxnsOysVaE4_ZcC9zCoNIbxYE.mMWcvnwcZJ.darnhxzlfTWtG',
 		redirectUri: 'https://metadatatoolkit.herokuapp.com/authorize.htm',
 		proxyUrl: 'https://mdtk-proxy.herokuapp.com/proxy/'
 	});
 	
-	conn = jsforce.browser.connection;
+	if(!jsforce.browser.isLoggedIn()){
+		$('#noLoginContainer').removeClass('slds-hide');
+	} 
+
+	jsforce.browser.on('connect', function(connection) {
+
+		conn = jsforce.browser.connection;
+
+		$('#overlay').addClass('slds-backdrop_open');
+		$('#spinner').removeClass('slds-hide');
+
+		console.log('Connecting to ' + conn.instanceUrl);
+		$('#noLoginContainer').addClass('slds-hide');
+
+		var userQuery = 'select Name, Username from User where Id = \'' + conn.userInfo.id + '\' limit 1';
+
+		conn.query(userQuery, function(err, res){
+			userInfo = res.records[0];
+			$('#userFullname').html(userInfo.Name);
+			$('#userUsername').html(' (' + userInfo.Username + ')');
+		})
+
+		.then(function(){
+			$('#overlay').removeClass('slds-backdrop_open');
+			$('#spinner').addClass('slds-hide');
+			$('#container').removeClass('slds-hide');
+		});
+	});
 }
 
 function login(instance) {
 	jsforce.browser.config.loginUrl = 'https://' + instance + '.salesforce.com';
 	jsforce.browser.login();
+}
+
+function logout(){
+	jsforce.browser.logout();
+	window.location.reload();
 }
 
 function setNavigation() {
