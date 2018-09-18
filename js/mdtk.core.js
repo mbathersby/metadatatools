ab0987d233256f84154d1b077008f930f864a44d
mbathersby <35317903+mbathersby@users.noreply.github.com>
2018-09-18 11:16:02 +1000

    Update mdtk.core.js

--- js/mdtk.core.js
+++ js/mdtk.core.js
@@ -14,33 +14,33 @@ function init() {
 	
 	if(!jsforce.browser.isLoggedIn()){
 		$('#noLoginContainer').removeClass('slds-hide');
-	} 
-
-	jsforce.browser.on('connect', function(connection) {
-
-		conn = jsforce.browser.connection;
-
-		$('#overlay').addClass('slds-backdrop_open');
-		$('#spinner').removeClass('slds-hide');
-
-		console.log('Connecting to ' + conn.instanceUrl);
-		$('#noLoginContainer').addClass('slds-hide');
-
-		var userQuery = 'select Name, Username from User where Id = \'' + conn.userInfo.id + '\' limit 1';
-
-		conn.query(userQuery, function(err, res){
-			userInfo = res.records[0];
-			$('#userFullname').html(userInfo.Name);
-			$('#userUsername').html(' (' + userInfo.Username + ')');
-		})
-
-		.then(function(){
-			$('#overlay').removeClass('slds-backdrop_open');
-			$('#spinner').addClass('slds-hide');
-			$('#container').removeClass('slds-hide');
-		});
-	});
+	}
 }
+
+jsforce.browser.on('connect', function(connection) {
+
+	conn = jsforce.browser.connection;
+
+	$('#overlay').addClass('slds-backdrop_open');
+	$('#spinner').removeClass('slds-hide');
+
+	console.log('Connecting to ' + conn.instanceUrl);
+	$('#noLoginContainer').addClass('slds-hide');
+
+	var userQuery = 'select Name, Username from User where Id = \'' + conn.userInfo.id + '\' limit 1';
+
+	conn.query(userQuery, function(err, res){
+		userInfo = res.records[0];
+		$('#userFullname').html(userInfo.Name);
+		$('#userUsername').html(' (' + userInfo.Username + ')');
+	})
+
+	.then(function(){
+		$('#overlay').removeClass('slds-backdrop_open');
+		$('#spinner').addClass('slds-hide');
+		$('#container').removeClass('slds-hide');
+	});
+});
 
 function login(instance) {
 	jsforce.browser.config.loginUrl = 'https://' + instance + '.salesforce.com';
