39594933ca5ba3ca12ab1d0c5d2ed7efa61036ff
mbathersby <35317903+mbathersby@users.noreply.github.com>
2018-09-18 10:38:49 +1000

    Update footer.php

--- footer.php
+++ footer.php
@@ -21,25 +21,9 @@
 					userInfo = res.records[0];
 					$('#userFullname').html(userInfo.Name);
 					$('#userUsername').html(' (' + userInfo.Username + ')');
-					}).then(function(res){
-					conn.describeGlobal(function(err, res) {
-						if (err) { return console.error(err); }
-						
-						mdObjs = [];
-						
-						for(key in res.sobjects){
-							var sObj = res.sobjects[key];
-							if(sObj.name.includes('__mdt')){
-								mdObjs.push(sObj);
-								
-								$('#object-select').append($("<option></option>")
-								.attr("value", sObj.name)
-								.text(sObj.label + ' (' + sObj.name + ')')
-								);
-							}
-						}
-					});
-					}).then(function(){
+				})
+					
+				.then(function(){
 					$('#overlay').removeClass('slds-backdrop_open');
 					$('#spinner').addClass('slds-hide');
 					$('#container').removeClass('slds-hide');
