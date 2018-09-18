9e63217b406a99c7ad2f64d5df0ea130a47f7cc1
mbathersby <35317903+mbathersby@users.noreply.github.com>
2018-09-18 15:54:35 +1000

    Update mdtk.package.js

--- js/mdtk.package.js
+++ js/mdtk.package.js
@@ -48,9 +48,9 @@ function setBaseXml(){
 				var mdTypeObj = mdTypeObjs[key];
 				console.log(mdTypeObj);
 				
-				/*mdTypeObj.sort(function(a, b) {
+				mdTypeObj.sort(function(a, b) {
 					return a.localeCompare(b);
-				});*/
+				});
 				
 				for(var j=0; j < mdTypeObj.length; j++){
 					typeString += '\n\t\t<members>' + mdTypeObj[j] + '</members>';
