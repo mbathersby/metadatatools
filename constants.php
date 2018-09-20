f4a1ab1eb139eb1ec5bdb3706756b1227f7338f0
Matthew Bathersby <matthew.bathersby@accenture.com>
2018-09-20 10:34:58 +1000

    no message

--- constants.php
+++ constants.php
@@ -1,11 +1,11 @@
 <?php
 
-	const CLIENT_ID 			= "3MVG9d8..z.hDcPI8U4xIar0rbAfGvpz7BlQxnsOysVaE4_ZcC9zCoNIbxYE.mMWcvnwcZJ.darnhxzlfTWtG";
-	const REDIRECT_URI 			= "https://mdtk.herokuapp.com/authorize.htm";
-	const PROXY_URL 			= "https://mdtk-proxy.herokuapp.com/proxy/");
-	const API_VERSION			= "43.0";
-	const MAX_PACKAGE_SIZE		= 39000;
-	const MAX_PACKAGE_ROWS 		= 10000;
+	define("CLIENT_ID", "3MVG9d8..z.hDcPI8U4xIar0rbAfGvpz7BlQxnsOysVaE4_ZcC9zCoNIbxYE.mMWcvnwcZJ.darnhxzlfTWtG");
+	define("REDIRECT_URI", "https://mdtk.herokuapp.com/authorize.htm");
+	define("PROXY_URL", "https://mdtk-proxy.herokuapp.com/proxy/");
+	define("API_VERSION", "43.0");
+	define("MAX_PACKAGE_SIZE", 39000);
+	define("MAX_PACKAGE_ROWS", 10000);
 	
 ?>
 	
