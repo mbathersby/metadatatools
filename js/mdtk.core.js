db6d93b0740b8aaf98b92db706c91c481a12825c
Matthew Bathersby <matthew.bathersby@accenture.com>
2018-09-20 11:16:20 +1000

    no message

--- js/mdtk.core.js
+++ js/mdtk.core.js
@@ -26,6 +26,8 @@ function init() {
 jsforce.browser.on('connect', function(connection) {
 
 	CONN = jsforce.browser.connection;
+	
+	CONN.version = API_VERSION;
 
 	$('#overlay').addClass('slds-backdrop_open');
 	$('#spinner').removeClass('slds-hide');
